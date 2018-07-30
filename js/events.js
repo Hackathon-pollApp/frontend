b77ed55757e12f5a3e50affda0b82ce62148f341
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-07-30 09:45:08 +0200

    formatted

--- js/events.js
+++ js/events.js
@@ -1,12 +1,11 @@
-
 // History back botton
 function goBack() {
     window.history.back();
 }
 
-var n=1;
-var m=2;
-var b=2;
+var n = 1;
+var m = 2;
+var b = 2;
 var element = document.querySelector("#content");
 
 var addOptionsButton = document.querySelector("#addOption");
@@ -17,117 +16,105 @@ var addVoterButton = document.querySelector("#addVoter");
 var addVoters = document.querySelector("#voters");
 
 
-
 function eventDisplayCreator() {
-    
+
     var event = document.createElement("div");
-    event.setAttribute("id","event"+n);
+    event.setAttribute("id", "event" + n);
     element.appendChild(event);
-    
+
     var eventImage = document.createElement("img");
-    eventImage.setAttribute("id","evImage");
+    eventImage.setAttribute("id", "evImage");
     event.appendChild(eventImage);
-    
+
     var evTitle = document.createElement("h2");
-    evTitle.setAttribute("id","evTitle");
+    evTitle.setAttribute("id", "evTitle");
     event.appendChild(evTitle);
-    
+
     var evText = document.createElement("p");
-    evText.setAttribute("id","evText");
+    evText.setAttribute("id", "evText");
     event.appendChild(evText);
     evText.classList.add("textEvent");
 
     var evHour = document.createElement("p");
-    evHour.setAttribute("id","evHour");
+    evHour.setAttribute("id", "evHour");
     event.appendChild(evHour);
     evHour.classList.add("horaEvent");
-    
-    
+
+
     ++n;
 // Calling the test function
-showData(eventImage,evTitle,evText,evHour);
+    showData(eventImage, evTitle, evText, evHour);
 }
 
 
 // Function to test the content in event list page 
-function showData(eventImage,evTitle,evText,evHour){
+function showData(eventImage, evTitle, evText, evHour) {
 
- eventImage.setAttribute("src","https://www.milton.ca/en/live/resources/Events.jpg");
- evTitle.innerHTML = "Convocatòria Ship2B per a startups amb impacte";
- evText.innerHTML = "Local Y";
- evHour.innerHTML = "vie, Oct.5 11:30";
-;
+    eventImage.setAttribute("src", "https://www.milton.ca/en/live/resources/Events.jpg");
+    evTitle.innerHTML = "Convocatòria Ship2B per a startups amb impacte";
+    evText.innerHTML = "Local Y";
+    evHour.innerHTML = "vie, Oct.5 11:30";
+    ;
 
- 
+
 }
 
 
+function optionCreator() {
 
-function optionCreator(){
-    
-var newOption = document.createElement("input");
+    var newOption = document.createElement("input");
 
-newOption.setAttribute("id","option"+m);
-newOption.setAttribute("type","text"); 
-newOption.setAttribute("value","Opció "+""+m);
-newOption.setAttribute("name","option"+""+m);      
-addOptions.appendChild(newOption);
-newOption.classList.add("optioncustom","text-center");
+    newOption.setAttribute("id", "option" + m);
+    newOption.setAttribute("type", "text");
+    newOption.setAttribute("value", "Opció " + "" + m);
+    newOption.setAttribute("name", "option" + "" + m);
+    addOptions.appendChild(newOption);
+    newOption.classList.add("optioncustom", "text-center");
     ++m;
-    
+
 }
 
-function voterCreator(){
-    var newVoter=document.createElement("form");
-    newVoter.setAttribute("id","voter"+b);
+function voterCreator() {
+    var newVoter = document.createElement("form");
+    newVoter.setAttribute("id", "voter" + b);
     addVoters.appendChild(newVoter);
 
-    var nameVoterInput=document.createElement("input");
-    nameVoterInput.setAttribute("class","voterName");
-    nameVoterInput.setAttribute("id","voterName"+b);
+    var nameVoterInput = document.createElement("input");
+    nameVoterInput.setAttribute("class", "voterName");
+    nameVoterInput.setAttribute("id", "voterName" + b);
     newVoter.appendChild(nameVoterInput);
-    nameVoterInput.classList.add("optioncustom","text-center");
- 
-    var emailVoterInput=document.createElement("input");
-    emailVoterInput.setAttribute("class","voterMail");
-    emailVoterInput.setAttribute("id","voterMail"+b);
-    newVoter.appendChild(emailVoterInput);
-    emailVoterInput.classList.add("optioncustom","text-center");
+    nameVoterInput.classList.add("optioncustom", "text-center");
 
-    var nVotesInput=document.createElement("input");
-    nVotesInput.setAttribute("class","votesNum");
-    nVotesInput.setAttribute("id","votesNum"+b);
+    var emailVoterInput = document.createElement("input");
+    emailVoterInput.setAttribute("class", "voterMail");
+    emailVoterInput.setAttribute("id", "voterMail" + b);
+    newVoter.appendChild(emailVoterInput);
+    emailVoterInput.classList.add("optioncustom", "text-center");
+
+    var nVotesInput = document.createElement("input");
+    nVotesInput.setAttribute("class", "votesNum");
+    nVotesInput.setAttribute("id", "votesNum" + b);
     newVoter.appendChild(nVotesInput);
-    nVotesInput.classList.add("optioncustom","text-center");
+    nVotesInput.classList.add("optioncustom", "text-center");
 
 }
 
 
-document.addEventListener("keypress",eventDisplayCreator,false);
-addOptionsButton.addEventListener("click",optionCreator,false);
-addVoterButton.addEventListener("click",voterCreator,false);
-
-
-
-
-
-
-
-
-
-
-
+document.addEventListener("keypress", eventDisplayCreator, false);
+addOptionsButton.addEventListener("click", optionCreator, false);
+addVoterButton.addEventListener("click", voterCreator, false);
 
 
 function handleFileSelect(evt) {
-    var files = evt.target.files; 
+    var files = evt.target.files;
     var output = [];
     for (var i = 0, f; f = files[i]; i++) {
-      output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
-                  f.size, ' bytes, last modified: ',
-                  f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
-                  '</li>');
+        output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+            f.size, ' bytes, last modified: ',
+            f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+            '</li>');
     }
     document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
-  }
-  document.getElementById('files').addEventListener('change', handleFileSelect, false);
+}
+
+document.getElementById('files').addEventListener('change', handleFileSelect, false);
--- js/login.js
+++ js/login.js
@@ -1,89 +1,90 @@
 //var url='https://poll-webapp.herokuapp.com';
-var url='http://localhost:8080';
+var url = 'http://localhost:8080';
 
 $('#registerDiv').show();
 $('#loginDiv').hide();
 
 
-$('#buttonLogin').click( function() {
-   var success=function(user){
-      alert("Benvingut " + user.name +", el teu id es " + user.id);
-   };
+$('#buttonLogin').click(function () {
+    var success = function (user) {
+        alert("Benvingut " + user.name + ", el teu id es " + user.id);
+    };
 
-   var error=function(error){
-      alert("Error al registrar-se: " + error.status);
-   };
+    var error = function (error) {
+        alert("Error al registrar-se: " + error.status);
+    };
 
-   var json = initLoginJson();
-   post(url+"/login",json,success,error);
-   
+    var json = initLoginJson();
+    post(url + "/login", json, success, error);
+
 });
 
-function initLoginJson(){
-   var email=$('#inputEmail').val();
-   var password=$('#inputPassword').val();
+function initLoginJson() {
+    var email = $('#inputEmail').val();
+    var password = $('#inputPassword').val();
 
-   var json={};
+    var json = {};
 
-   json["email"]=email;
-   json["password"]=password;
-   //type, entity or user
-   json["type"]="entity";
+    json["email"] = email;
+    json["password"] = password;
+    //type, entity or user
+    json["type"] = "entity";
 
-   return json;
-}
-$('#registerButton').click( function() {
-   var success=function(user){
-      alert("Benvingut " + user.name +", el teu id es " + user.id);
-   };
-
-   var error=function(error){
-      alert("Error al registrar-se: " + error.status);
-   };
-
-   var json = initRegisterJson();
-   debugger;
-   post(url+"/users",json,success,error);
-});
-
-function initRegisterJson(){
-   var email=$('#inputEmailRegister').val();
-   var name=$('#inputNameRegister').val();
-   var password=$('#inputPasswordRegister').val();
-
-   var json={};
-
-   json["email"]=email;
-   json["name"]=name;
-   json["password"]=password;
-
-   return json;
+    return json;
 }
 
+$('#registerButton').click(function () {
+    var success = function (user) {
+        alert("Benvingut " + user.name + ", el teu id es " + user.id);
+    };
 
-$('#goToLogin').click( function() {
+    var error = function (error) {
+        alert("Error al registrar-se: " + error.status);
+    };
 
-   $('#registerDiv').hide();
-   $('#loginDiv').show();
+    var json = initRegisterJson();
+    debugger;
+    post(url + "/users", json, success, error);
+});
+
+function initRegisterJson() {
+    var email = $('#inputEmailRegister').val();
+    var name = $('#inputNameRegister').val();
+    var password = $('#inputPasswordRegister').val();
+
+    var json = {};
+
+    json["email"] = email;
+    json["name"] = name;
+    json["password"] = password;
+
+    return json;
+}
+
+
+$('#goToLogin').click(function () {
+
+    $('#registerDiv').hide();
+    $('#loginDiv').show();
 });
 
 
-$('#goToRegisterButton').click( function() {
+$('#goToRegisterButton').click(function () {
 
-   $('#registerDiv').show();
-   $('#loginDiv').hide();
+    $('#registerDiv').show();
+    $('#loginDiv').hide();
 });
 
 
-function post(url,json,success, error){
-   $.ajax({
-     method: "POST",
-     url: url,
-     data: JSON.stringify(json),
-     contentType: "application/json"
-   }).done(function(data) {
-      success(data);
-   }).fail(function(err){
-      error(err);
-   });
+function post(url, json, success, error) {
+    $.ajax({
+        method: "POST",
+        url: url,
+        data: JSON.stringify(json),
+        contentType: "application/json"
+    }).done(function (data) {
+        success(data);
+    }).fail(function (err) {
+        error(err);
+    });
 }
