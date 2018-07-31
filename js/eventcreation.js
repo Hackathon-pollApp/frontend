2ebb4b9f0f12f8e4532d65873b1a6ef8de0621ba
Minnacava <marinacamachovarea@outlook.com>
2018-07-31 11:41:02 +0200

    reset

--- js/eventcreation.js
+++ js/eventcreation.js
@@ -28,8 +28,7 @@ class eventCreator{
     showEvents(){	
          for(let product of this.data){	
         let div1 = document.createElement("div");	
-        div1.setAttribute("class","clicable");
-        div1.setAttribute("onclick","eventId(product.id)")
+        div1.setAttribute("id","clicable0");
         div1.setAttribute("class",product.id); 
          div1.setAttribute("class","event",product.id);
          div1.setAttribute("data-event-id",product.id);	
@@ -38,20 +37,20 @@ class eventCreator{
     let eventImage = document.createElement("img");	
     eventImage.setAttribute("src",product.eventImage);	
     eventImage.setAttribute("class",product.id); 
-    eventImage.setAttribute("class","clicable");
+    eventImage.setAttribute("id",'clicable1'); 
     div1.appendChild(eventImage);	
     	
     let evTitle = document.createElement("h2");	
     evTitle.innerHTML=product.evTitle;	
-    evTitle.setAttribute("class",id); 
-    evTitle.setAttribute("class","clicable");
+    evTitle.setAttribute("class",product.id); 
+    evTitle.setAttribute("id",'clicable2');
     div1.appendChild(evTitle);	
     evTitle.classList.add("evTitle");
     	
     let evText = document.createElement("p");	
     evText.innerHTML=product.evText;	
     evText.setAttribute("class",product.id); 
-    evText.setAttribute("class","clicable");
+    evText.setAttribute("id",'clicable3');
     div1.appendChild(evText);	
     evText.classList.add("evText");	
 
@@ -69,26 +68,23 @@ function loadBestSell(data){
     let myItem = new eventCreator(data);	
     myItem.showEvents();	
 }	
-
-function eventId(id){
-
-    console.log(id);
-}
 var data2 = loadBestSell(data);	
-    
 
-    // $(".clicable").each(function(){
-    //     debugger;
-    //     var DOMelement = this;
-    //     var jQueryElement = $(this);
+ document.getElementById("clicable0").addEventListener("click",function(evt)
+ {	
+    $("clicable0").each(function(){
+        debugger;
+        var DOMelement = this;
+        var jQueryElement = $(this);
         
-    // });
+    });
 
-  //  $("clicable").parent()
-    //  console.log(evt.target);
+    $("clicable").parent()
+     console.log(evt.target);
 //     console.log(evt.target.classList);
     //  window.location.replace("eventSinglePage.html")	
 
+})
 // SinglePage
 function searchEvent(id){
     var eventFound = null; 
